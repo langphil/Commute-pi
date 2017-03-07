d8f695b357a1435fa75fe9f28b90090660fb4e3c
Phil Lang <plangphoto@gmail.com>
2017-03-07 19:34:40 +0000

    looping through direction of travel makes more sense

--- dart.py
+++ dart.py
@@ -10,50 +10,25 @@ response = requests.get(url)
 train = ElementTree.fromstring(response.content)
 
 
-# Looping and printing all Northside trains
-def direction(final_stop):
+# Looping and printing all trains stopping at Bayside going both directions
+def direction(line):
 
-    for x in train: # THIS GETS ME ALL THE PATIENT ATTRIBUTES
-        origin = x[6].text
+    for x in train:
         destination = x[7].text
+        dueIn = x[12].text
         location = x[11].text
         sched = x[17].text
         est = x[15].text
-        dueIn = x[12].text
+        direction = x[18].text
 
-        if destination == final_stop:
+        if direction == line:
             print 'Destination: ' + destination
-            print 'Train is due in ' + dueIn + ' mins'
+            print 'Due in ' + dueIn + ' mins'
             print location
             print 'Scheduled: ' + sched
             print 'Estimated: ' + est
             print
 
 
-direction('Bray')
-direction('Howth')
-
-
-# DEPRECATED CODE
-
-
-# Parse XML
-station = train[0][2].text
-destination = train[0][7].text
-nextTrainSched = train[0][17].text
-nextTrainEst = train[0][15].text
-justLeft = train[0][11].text
-
-
-# Check whether the Dart will arrive at it's scheduled time
-# Add a counter here to add up how late the Dart is
-# Dart can be early
-def onTime():
-    if nextTrainEst == nextTrainSched:
-        print 'Dart is on time'
-    else:
-        print 'Dart is not on time: originally scheduled for ' + nextTrainSched
-
-
-#print 'The next Dart to ' + destination + ' leaves from ' + station + ' is at ' + nextTrainEst + '. It has just ' + justLeft + '.'
-#onTime()
+direction('Southbound')
+direction('Northbound')
