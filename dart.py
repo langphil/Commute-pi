5ee6d4f16f41f67b3ceb89ed88de7980bd3b979c
Phil <plangphoto@gmail.com>
2017-03-08 15:15:46 +0000

    pep8 clean-up

--- dart.py
+++ dart.py
@@ -1,18 +1,21 @@
 # Check Dart departure times for Bayside
-import requests, json
+import requests
+import json
 from datetime import datetime
 from xml.etree import ElementTree
 
 
 # Call and parse XML file
-url = 'http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByNameXML?StationDesc=Bayside'
+url = 'http://api.irishrail.ie/realtime/realtime.asmx/' \
+      'getStationDataByNameXML?StationDesc=Bayside'
 response = requests.get(url)
 train = ElementTree.fromstring(response.content)
 
 
 # Looping and printing all trains stopping at Bayside going both directions
-def direction(line):
 
+
+def direction(line):
     for x in train:
         destination = x[7].text
         dueIn = x[12].text
